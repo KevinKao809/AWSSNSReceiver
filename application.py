d8a463479eca4fd46637802a141af21a42e2c2e1
Kevin CY Kao (高啟原) <KevinCY_Kao@moxa.com>
2018-11-08 18:41:23 +0800

    u6

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, abort
 from azure.storage.blob import AppendBlobService
 from datetime import datetime
-import requests
+import requests, json
 app = Flask(__name__)
 
 storageService = AppendBlobService(connection_string='DefaultEndpointsProtocol=https;AccountName=thingspronotify;AccountKey=axD13Z8R9WkPSi7mcdRUtnPfx9skMYfjf3D/vA92tcs21TOSqcJMHfc4TBvribk4Ed09kNc0EIgct8lJzYRe6w==;EndpointSuffix=core.windows.net')
@@ -22,7 +22,7 @@ def receiver():
             writeToAppendBlob('333')
             if AWS_MESSAGE_TYPE == 'SubscriptionConfirmation':
                 writeToAppendBlob('444')
-                postData = request.get_json(silent=True)
+                postData = json.loads(request.data)
                 writeToAppendBlob('555')
                 subscribeURL = postData['SubscribeURL']
                 writeToAppendBlob('666')
